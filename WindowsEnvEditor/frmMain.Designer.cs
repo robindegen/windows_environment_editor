c1af5e5ea2c6cf342e57e885cfbd2b6303c8ba70
Robin Degen <robindegen@gmail.com>
2014-08-26 15:58:54 +0200

    Initial commit

---
+++ WindowsEnvEditor/frmMain.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace WindowsEnvEditor
+{
+	partial class frmMain
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.lstEnvVars = new System.Windows.Forms.ListBox();
+			this.lstEnvVarValues = new System.Windows.Forms.ListBox();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.rdoSystem = new System.Windows.Forms.RadioButton();
+			this.rdoUser = new System.Windows.Forms.RadioButton();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// lstEnvVars
+			// 
+			this.lstEnvVars.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+			this.lstEnvVars.FormattingEnabled = true;
+			this.lstEnvVars.Location = new System.Drawing.Point(12, 80);
+			this.lstEnvVars.Name = "lstEnvVars";
+			this.lstEnvVars.Size = new System.Drawing.Size(262, 355);
+			this.lstEnvVars.TabIndex = 0;
+			this.lstEnvVars.SelectedIndexChanged += new System.EventHandler(this.lstEnvVars_SelectedIndexChanged);
+			// 
+			// lstEnvVarValues
+			// 
+			this.lstEnvVarValues.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.lstEnvVarValues.FormattingEnabled = true;
+			this.lstEnvVarValues.Location = new System.Drawing.Point(280, 15);
+			this.lstEnvVarValues.Name = "lstEnvVarValues";
+			this.lstEnvVarValues.Size = new System.Drawing.Size(475, 420);
+			this.lstEnvVarValues.TabIndex = 1;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.rdoSystem);
+			this.groupBox1.Controls.Add(this.rdoUser);
+			this.groupBox1.Location = new System.Drawing.Point(12, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(262, 62);
+			this.groupBox1.TabIndex = 2;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Scope";
+			// 
+			// rdoSystem
+			// 
+			this.rdoSystem.AutoSize = true;
+			this.rdoSystem.Location = new System.Drawing.Point(6, 36);
+			this.rdoSystem.Name = "rdoSystem";
+			this.rdoSystem.Size = new System.Drawing.Size(59, 17);
+			this.rdoSystem.TabIndex = 1;
+			this.rdoSystem.Text = "System";
+			this.rdoSystem.UseVisualStyleBackColor = true;
+			this.rdoSystem.CheckedChanged += new System.EventHandler(this.rdoSystem_CheckedChanged);
+			// 
+			// rdoUser
+			// 
+			this.rdoUser.AutoSize = true;
+			this.rdoUser.Checked = true;
+			this.rdoUser.Location = new System.Drawing.Point(6, 16);
+			this.rdoUser.Name = "rdoUser";
+			this.rdoUser.Size = new System.Drawing.Size(47, 17);
+			this.rdoUser.TabIndex = 0;
+			this.rdoUser.TabStop = true;
+			this.rdoUser.Text = "User";
+			this.rdoUser.UseVisualStyleBackColor = true;
+			this.rdoUser.CheckedChanged += new System.EventHandler(this.rdoUser_CheckedChanged);
+			// 
+			// frmMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(767, 446);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.lstEnvVarValues);
+			this.Controls.Add(this.lstEnvVars);
+			this.Name = "frmMain";
+			this.Text = "Environment Editor";
+			this.Load += new System.EventHandler(this.frmMain_Load);
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListBox lstEnvVars;
+		private System.Windows.Forms.ListBox lstEnvVarValues;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.RadioButton rdoSystem;
+		private System.Windows.Forms.RadioButton rdoUser;
+	}
+}
+
---
+++ WindowsEnvEditor/frmMain.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsEnvEditor
+{
+	public partial class frmMain : Form
+	{
+		public frmMain()
+		{
+			InitializeComponent();
+		}
+
+		private void frmMain_Load(object sender, EventArgs e)
+		{
+			__fill_env_vars_list(EnvironmentVariableTarget.User);
+		}
+
+		private void lstEnvVars_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			string envvar_name = (string) lstEnvVars.SelectedItem;
+			EnvironmentVariableTarget target = EnvironmentVariableTarget.Machine;
+
+			if(rdoUser.Checked)
+				target = EnvironmentVariableTarget.User;
+
+			__file_env_vars_value(target, envvar_name);
+		}
+
+		private void rdoUser_CheckedChanged(object sender, EventArgs e)
+		{
+			__fill_env_vars_list(EnvironmentVariableTarget.User);
+		}
+
+		private void rdoSystem_CheckedChanged(object sender, EventArgs e)
+		{
+			//In the windows settings this is called system
+			__fill_env_vars_list(EnvironmentVariableTarget.Machine);
+		}
+
+		void __fill_env_vars_list(EnvironmentVariableTarget target)
+		{
+			lstEnvVars.Items.Clear();
+
+			foreach (DictionaryEntry de in Environment.GetEnvironmentVariables(target))
+			{
+				lstEnvVars.Items.Add(de.Key);
+			}
+
+			if(lstEnvVars.Items.Count > 0)
+				lstEnvVars.SelectedIndex = 0;
+		}
+
+		void __file_env_vars_value(EnvironmentVariableTarget target, string varname)
+		{
+			lstEnvVarValues.Items.Clear();
+
+			if(varname == null)
+				return;
+
+			string value = Environment.GetEnvironmentVariable(varname, target);
+
+			if (value == null)
+				return;
+			
+			string[] value_split = value.Split(new char[] { ';' });
+
+			foreach (string val in value_split)
+			{
+				lstEnvVarValues.Items.Add(val);
+			}
+			return;
+		}
+	}
+}
